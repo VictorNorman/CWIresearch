2a472a5132362fd87ed23f224fc5532307bdc23e
AJ Vrieland <zer0relm@gmail.com>
2021-06-29 15:44:55 -0400

    Added executables, and updated FileTests

--- seismicRefraction/fileTests.py
+++ seismicRefraction/fileTests.py
@@ -3,18 +3,38 @@ from tkinter.filedialog import askopenfilename
 
 Tk().withdraw()
 
-base_file = askopenfilename(filetypes=[("csv files", "*.csv;*.CSV")])
-compare_file = askopenfilename(filetypes=[("csv files", "*.csv;*.CSV")])
+reference_file1 = open("Reference_data1.csv","r")
+reference_file2 = open("Reference_data2.csv","r") 
+reference_file3 = open("Reference_data3.csv","r") 
+test_file1 = open("Test_data1.csv","r")
+test_file2 = open("Test_data2.csv","r")
+test_file3 = open("Test_data3.csv","r")
 
-csvFile = open(base_file,"r") 
-csvFile2 = open(compare_file,"r")
+reference_data1 = reference_file1.readlines()
+reference_data2 = reference_file2.readlines()
+reference_data3 = reference_file3.readlines()
+test_data1 = test_file1.readlines()
+test_data2 = test_file2.readlines()
+test_data3 = test_file3.readlines()
 
-data1 = csvFile.readlines()
-data2 = csvFile2.readlines()
 
-if len(data1) == len(data2):
-    for i in range(len(data1)):
-        if data1[i] != data2[i]:
+if len(reference_data1) == len(test_data1):
+    for i in range(len(reference_data1)):
+        if reference_data1[i] != test_data1[i]:
+            raise Exception("not equal")
+else:
+    raise Exception("different lengths")
+
+if len(reference_data2) == len(test_data2):
+    for i in range(len(reference_data2)):
+        if reference_data2[i] != test_data2[i]:
+            raise Exception("not equal")
+else:
+    raise Exception("different lengths")
+
+if len(reference_data3) == len(test_data3):
+    for i in range(len(reference_data3)):
+        if reference_data3[i] != test_data3[i]:
             raise Exception("not equal")
 else:
     raise Exception("different lengths")
